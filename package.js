ab08a26de59419790da55d5eb256962b9db0495f
Chris Mather <mather.chris@gmail.com>
2013-09-10 18:07:32 -0700

    Tests and minor adjustments for lib/route.js

--- package.js
+++ package.js
@@ -3,82 +3,53 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use([
-    'reactive-dict',
-    'meteor',
-    'deps',
-    'underscore',
-    'ejson'
-  ], ['client', 'server']);
+  api.use('reactive-dict', ['client', 'server']);
+  api.use('deps', ['client', 'server']);
+  api.use('underscore', ['client', 'server']);
+  api.use('ejson', ['client', 'server']);
 
-  api.use([
-    'templating',
-    'handlebars',
-    'jquery'
-  ], 'client');
+  api.use('templating', 'client');
+  api.use('handlebars', 'client', {weak: true});
+  api.use('jquery', 'client');
 
-  api.add_files([
-    'lib/utils.js',
-    'lib/route.js',
-    'lib/route_controller.js',
-    'lib/router.js'
-  ], ['client', 'server']);
+  api.add_files('lib/utils.js', ['client', 'server']);
+  api.add_files('lib/route.js', ['client', 'server']);
+  api.add_files('lib/route_controller.js', ['client', 'server']);
+  api.add_files('lib/router.js', ['client', 'server']);
 
-  api.add_files([
-    'lib/client/location.js',
-    'lib/client/page_controller.js',
-    'lib/client/router.js',
-    'lib/client/default_layout.html',
-    'lib/client/route_controller.js',
-    'lib/client/helpers.js'
-  ], 'client');
+  api.add_files('lib/client/location.js', 'client');
+  api.add_files('lib/client/page_controller.js', 'client');
+  api.add_files('lib/client/router.js', 'client');
+  api.add_files('lib/client/default_layout.html', 'client');
+  api.add_files('lib/client/route_controller.js', 'client');
+  api.add_files('lib/client/helpers.js', 'client');
 
-  api.add_files([
-    'lib/server/route_controller.js',
-    'lib/server/router.js'
-  ], 'server');
+
+  api.add_files('lib/server/route_controller.js', 'server');
+  api.add_files('lib/server/router.js', 'server');
 
   // for backward compat before Meteor linker changes
   if (typeof api.export !== 'undefined') {
     api.use('webapp', 'server');
     Npm.depends({connect: '2.7.10'});
 
-    api.export([
-     'RouteController',
-     'Router'
-    ], ['client', 'server']);
+    api.export('RouteController', ['client', 'server']);
+    api.export('Route', ['client', 'server']);
+    api.export('Router', ['client', 'server']);
+    api.export('IronLocation', 'client');
 
-    api.export([
-      'IronLocation'
-    ], 'client');
-
-    api.export([
-     'Utils'
-    ], ['client', 'server'], {testOnly: true});
+    api.export('Utils', ['client', 'server'], {testOnly: true});
   }
 });
 
 Package.on_test(function (api) {
-  api.use([
-    'iron-router',
-    'tinytest',
-    'test-helpers',
-    'templating',
-    'reactive-dict'
-  ], ['client']);
+  api.use('iron-router', ['client', 'server']);
+  api.use('tinytest', ['client', 'server']);
+  api.use('test-helpers', ['client', 'server']);
+  api.use('reactive-dict', ['client', 'server']);
 
-  api.add_files([
-    'test/test_helpers.js',
-    'test/router_utils_test.js',
-    'test/route_path_test.js',
-    'test/route_context_test.js',
-    'test/route_test.js',
-    'test/router_test.js',
-  ], ['client']);
+  api.use('templating', 'client');
 
-  api.add_files([
-    'test/client/test_templates.html',
-    'test/client/route_controller_test.js',
-    'test/client/client_router_test.js'
-  ], 'client');
+  api.add_files('test/test_helpers.js', ['client', 'server']);
+  api.add_files('test/route.js', ['client', 'server']);
 });
--- test/test_helpers.js
+++ test/test_helpers.js
@@ -2,13 +2,3 @@ Router.configure({
   autoRender: false,
   autoStart: false
 });
-
-if (Meteor.isServer) {
-  Meteor.publish('/posts', function () {
-    this.ready();
-  });
-
-  Meteor.publish('/posts/show', function (id) {
-    this.ready();
-  });
-}
