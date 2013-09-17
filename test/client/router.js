1a72edfe159b20783c48c0eb68a7313eb3dbe953
Chris Mather <mather.chris@gmail.com>
2013-09-17 13:06:23 -0700

    Initial tests for ClientRouter.
    - TODO: The layout and each yielded template should have the option to opt-in to
      the data context. That way, it doesn't re-render if it doesn't need to.
    - @tmeasday, right now, the layout and all yielded templates will re-render any
      time the router's data context changes, unless there is NO data context or the
      data context stays the same.
    - A more likely scenario is the data context changes, but the user doesn't want
      the layout to re-render, just the yielded templates which would re-render
      anyway.

--- test/client/router.js
+++ test/client/router.js
@@ -96,8 +96,94 @@ Tinytest.add('ClientRouter - run with computations', function (test) {
   test.equal(runCount, 1, 'prev controller was run again');
 });
 
+// XXX these tests should mostly be in a separate PageManager test, but to get
+// us started I'll just put the big tests here. We can break it out later.
 Tinytest.add('ClientRouter - rendering', function (test) {
-  test.ok();
+  var router = new ClientRouter({
+    autoRender: false,
+    autoStart: false
+  });
+
+  var div = new OnscreenDiv(router.render());
+
+  router.setLayout('layout');
+  Deps.flush();
+  test.equal(div.text().trim(), 'layout');
+
+  // render the main template
+  router.setTemplate('one');
+  Deps.flush();
+  test.equal(div.node().children[1].innerText.trim(), 'one');
+
+  // render yield templates
+  router.setTemplate('aside', 'aside');
+  router.setTemplate('footer', 'footer');
+  Deps.flush();
+  test.equal(div.node().children[0].innerText.trim(), 'aside');
+  test.equal(div.node().children[2].innerText.trim(), 'footer');
+
+  // change the main template
+  router.setTemplate('two');
+  Deps.flush();
+  test.equal(div.node().children[1].innerText.trim(), 'two');
+
+  div.kill();
+});
+
+Tinytest.add('ClientRouter - rendering with data', function (test) {
+  var router = new ClientRouter({
+    autoRender: false,
+    autoStart: false
+  });
+
+  var div = new OnscreenDiv(router.render());
+
+  var _layout = Template.layout;
+  var layoutRenderCount = 0;
+  Template.layout = function () {
+    layoutRenderCount++;
+    return _layout.apply(this, arguments);
+  };
+
+  router.setLayout('layout');
+  Deps.flush();
+  test.equal(div.text().trim(), 'layout');
+  test.equal(layoutRenderCount, 1);
+
+  router.setData({
+    text: 'one'
+  });
+
+  router.setTemplate('data');
+  router.setTemplate('data', 'aside');
+  router.setTemplate('data', 'footer');
+  Deps.flush();
+
+  test.equal(div.node().children[0].innerText.trim(), 'one');
+  test.equal(div.node().children[1].innerText.trim(), 'one');
+  test.equal(div.node().children[2].innerText.trim(), 'one');
+
+  router.setData({
+    text: 'two'
+  });
+
+  //XXX This should be fixed so the layout isn't rendered every time you set the
+  //global data context. The idea of the global data context is:
+  //
+  //1) many times all templates want to share a data context so you don't want
+  //to call a data function for every render
+  //
+  //2) if there is no data context you should be able to not set it, thereby not
+  //forcing a redraw of existing rendered templates
+  //
+  //3) what's needed is ability to opt out of data on a per template basis,
+  //either in the template or layout. So the render and setLayout methods should
+  //have an option for useData: false and by default the layout could have this
+  //property set. It could be overridden in the route.
+  Deps.flush();
+  test.equal(div.node().children[0].innerText.trim(), 'two');
+  test.equal(div.node().children[1].innerText.trim(), 'two');
+  test.equal(div.node().children[2].innerText.trim(), 'two');
 });
 
 Tinytest.add('ClientRouter - go', function (test) {
