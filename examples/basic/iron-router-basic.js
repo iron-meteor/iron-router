0ad8174659d1d828f85e94efcecd54ab7448d63a
Tom Coleman <tom@thesnail.org>
2014-02-24 17:54:44 +1100

    Switched click event handling to the router.
    For #319

--- examples/basic/iron-router-basic.js
+++ examples/basic/iron-router-basic.js
@@ -1,6 +1,12 @@
 Router.map(function() {
   this.route('one', {path: '/'});
   this.route('two');
+  this.route('three', {
+    where: 'server',
+    action: function() {
+      this.response.end('SERVER ROUTE');
+    }
+  });
 })
 
 if (Meteor.isClient) {
