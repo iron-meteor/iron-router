d9ee36556995b0a3a37e11cbeaac55cdf5b866f3
Chris Mather <mather.chris@gmail.com>
2014-03-17 11:32:19 -0700

    Tests for controller computation isolation.

--- test/client/route_controller.js
+++ test/client/route_controller.js
@@ -150,5 +150,88 @@ Tinytest.add('Client RouteController - _run order', function (test) {
   ], 'run order seems wrong');
 });
 
-Tinytest.add('Client RouteController - _run computations', function (test) {
+Tinytest.add('Client RouteController - _run then stop', function (test) {
+  var c = createController();
+  c._run();
+  test.isFalse(c._computation.stopped, "doesn't look like the controller's computation si running.");
+  c.stop();
+  test.isTrue(c._computation.stopped, "stop() didn't stop the controller's computation.");
+});
+
+Tinytest.add('Client RouteController - _run computation isolation', function (test) {
+  var calls = {};
+  var totalCalls = 0;
+  var logCall = function (name) {
+    totalCalls++;
+    calls[name] = calls[name] || 0;
+    calls[name]++;
+  };
+
+  var deps = {};
+  _.each(['onRun', 'waitOn', 'data', 'action', 'onStop'], function (name) {
+    deps[name] = new Deps.Dependency;
+  });
+
+  var c = createController({
+    onRun: function () {
+      deps.onRun.depend();
+      logCall('onRun');
+    },
+
+    waitOn: function () {
+      deps.waitOn.depend();
+      logCall('waitOn');
+    },
+
+    data: function () {
+      deps.data.depend();
+      logCall('data');
+    },
+
+    onBeforeAction: function () {
+      logCall('onBeforeAction');
+    },
+
+    action: function () {
+      logCall('action');
+      deps.action.depend();
+    },
+
+    onAfterAction: function () {
+      logCall('onAfterAction');
+    }
+  });
+
+  c._run();
+
+  // everything but onStop
+  test.equal(totalCalls, 6, 'first run');
+
+  // now invalidate the onRun dep
+  deps.onRun.changed();
+  Deps.flush();
+  test.equal(totalCalls, 7, 'only onRun should have rerun');
+  test.equal(calls.onRun, 2, 'onRun should rerun');
+
+  // okay now the waitOn function!
+  deps.waitOn.changed();
+  Deps.flush();
+  test.equal(totalCalls, 8, 'only waitOn should have rerun');
+  test.equal(calls.waitOn, 2, 'waitOn should rerun');
+
+  // whoop whoop, now the data function!
+  deps.data.changed();
+  Deps.flush();
+  test.equal(totalCalls, 9, 'only data should have rerun');
+  test.equal(calls.data, 2, 'data should rerun');
+
+  // almost! now the action functions!
+  deps.action.changed();
+  Deps.flush();
+  // 9 to 12 is because action invalidtes
+  // onBeforeAction, action, onAfterAction
+  test.equal(totalCalls, 12, 'only action should have rerun');
+  test.equal(calls.action, 2, 'action should rerun');
+  test.equal(calls.onBeforeAction, 2, 'onBeforeAction should rerun');
+  test.equal(calls.onAfterAction, 2, 'onAfterAction should rerun');
 });
