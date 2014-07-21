ebc61ad222d8507c0ccba1298a68edefce8ff34d
Chris Mather <mather.chris@gmail.com>
2014-07-20 22:05:07 -0700

    clean up loading_plugin example

--- examples/loading_plugin/loading_plugin.js
+++ examples/loading_plugin/loading_plugin.js
@@ -1,16 +1,27 @@
 Router.configure({
-  // optionally configure a template to use
-  // the default loading text to use is "Loading..."
+  // Optionally configure a template to use.
+  // The default loading text to use is "Loading..."
   loadingTemplate: 'Loading'
 });
 
-Router.plugin('loading', {only: 'home'});
+Router.plugin('loading');
 
-Router.route('/', {
-  name: 'home',
+Router.route('/', function () {
+  this.render('Home');
+}, {
   waitOn: function () {
-    return {
-      ready: function () { return Session.get('ready'); }
-    };
+    // the loading plugin will render the loading template
+    // until this subscription is ready
+    return Meteor.subscribe('items');
   }
 });
+
+if (Meteor.isServer) {
+  Meteor.publish('items', function () {
+    var self = this;
+    // send the "ready" message in 2 seconds.
+    setTimeout(function () {
+      self.ready();
+    }, 2000);
+  });
+}
