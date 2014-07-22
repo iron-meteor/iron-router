5d9503dc1a361bddff98f25f4c959b4b148bbbff
Chris Mather <mather.chris@gmail.com>
2014-07-22 11:30:29 -0700

    change collectHooks to _collectHooks and update tests

--- test/route_controller_test.js
+++ test/route_controller_test.js
@@ -1,20 +1,33 @@
-Tinytest.add('RouteController - collectHooks', function (test) {
+Tinytest.add('RouteController - runHooks', function (test) {
   var router = new Iron.Router({autoStart: false, autoRender: false});
+  var hookCalls = [];
 
   router.configure({
-    onBeforeAction: function routerOnBeforeAction() {},
-    before: function routerBefore() {}
+    onBeforeAction: function routerOnBeforeAction() {
+      hookCalls.push('routerOnBeforeAction');
+    },
+    before: function routerBefore() {
+      hookCalls.push('routerBefore');
+    }
   });
 
   var C = Iron.RouteController.extend({
-    onBeforeAction: function protoOnBeforeAction() {},
-    before: function protoBefore() {}
+    onBeforeAction: function protoOnBeforeAction() {
+      hookCalls.push('protoOnBeforeAction');
+    },
+    before: function protoBefore() {
+      hookCalls.push('protoBefore');
+    }
   });
 
   var route = router.route('/', {
     controller: C,
-    onBeforeAction: function routeOnBeforeAction() {},
-    before: function routeBefore() {}
+    onBeforeAction: function routeOnBeforeAction() {
+      hookCalls.push('routeOnBeforeAction');
+    },
+    before: function routeBefore() {
+      hookCalls.push('routeBefore');
+    }
   });
 
   Iron.Router.hooks.testHook = function () {};
@@ -24,14 +37,12 @@ Tinytest.add('RouteController - collectHooks', function (test) {
   c.router = router;
   c.route = route;
 
-  var hooks = c.collectHooks('onBeforeAction', 'before');
+  var hooks = c.runHooks('onBeforeAction', 'before');
 
-  var hookNames = _.map(hooks, function (h) { return h.name; });
-
-  test.equal(hookNames[0], 'routerOnBeforeAction', 'router onBeforeAction');
-  test.equal(hookNames[1], 'routerBefore', 'router before');
-  test.equal(hookNames[2], 'routeOnBeforeAction', 'route onBeforeAction');
-  test.equal(hookNames[3], 'routeBefore', 'route before');
-  test.equal(hookNames[4], 'protoOnBeforeAction', 'proto onBeforeAction');
-  test.equal(hookNames[5], 'protoBefore', 'proto before');
+  test.equal(hookCalls[0], 'routerOnBeforeAction', 'router onBeforeAction');
+  test.equal(hookCalls[1], 'routerBefore', 'router before');
+  test.equal(hookCalls[2], 'routeOnBeforeAction', 'route onBeforeAction');
+  test.equal(hookCalls[3], 'routeBefore', 'route before');
+  test.equal(hookCalls[4], 'protoOnBeforeAction', 'proto onBeforeAction');
+  test.equal(hookCalls[5], 'protoBefore', 'proto before');
 });
