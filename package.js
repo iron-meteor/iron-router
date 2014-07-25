b5f0409859e179a6374aaa933d52ed09d79fadf9
Chris Mather <mather.chris@gmail.com>
2014-07-25 16:25:28 -0700

    change package.js to work with old package system for now

--- package.js
+++ package.js
@@ -16,35 +16,35 @@ Package.on_use(function (api) {
   api.use('meteor')
 
   // main namespace and utils
-  api.use('iron:core');
-  api.imply('iron:core');
+  api.use('iron-core');
+  api.imply('iron-core');
 
   // connect like middleware stack for client/server
-  api.use('iron:middleware-stack');
+  api.use('iron-middleware-stack');
 
   // client and server side url utilities and compiling
-  api.use('iron:url');
+  api.use('iron-url');
 
   // for reactive urls and pushState in the browser
-  api.use('iron:location');
+  api.use('iron-location');
 
   // for RouteController which inherits from this
-  api.use('iron:controller');
+  api.use('iron-controller');
 
-  api.addFiles('lib/current_options.js');
-  api.addFiles('lib/http_methods.js');
-  api.addFiles('lib/route_controller.js');
-  api.addFiles('lib/route_controller_server.js', 'server');
-  api.addFiles('lib/route_controller_client.js', 'client');
-  api.addFiles('lib/route.js');
-  api.addFiles('lib/router.js');
-  api.addFiles('lib/router_client.js', 'client');
-  api.addFiles('lib/router_server.js', 'server');
-  api.addFiles('lib/hooks_client.js', 'client');
-  api.addFiles('lib/helpers_client.js', 'client');
-  api.addFiles('lib/plugins.js');
-  api.addFiles('lib/global_router.js');
-  api.addFiles('lib/templates.html');
+  api.add_files('lib/current_options.js');
+  api.add_files('lib/http_methods.js');
+  api.add_files('lib/route_controller.js');
+  api.add_files('lib/route_controller_server.js', 'server');
+  api.add_files('lib/route_controller_client.js', 'client');
+  api.add_files('lib/route.js');
+  api.add_files('lib/router.js');
+  api.add_files('lib/router_client.js', 'client');
+  api.add_files('lib/router_server.js', 'server');
+  api.add_files('lib/hooks_client.js', 'client');
+  api.add_files('lib/helpers_client.js', 'client');
+  api.add_files('lib/plugins.js');
+  api.add_files('lib/global_router.js');
+  api.add_files('lib/templates.html');
 
   // symbol exports
   api.export('Router');
@@ -52,12 +52,12 @@ Package.on_use(function (api) {
 });
 
 Package.on_test(function (api) {
-  api.use('iron:router');
+  api.use('iron-router');
   api.use('tinytest');
   api.use('test-helpers');
 
-  api.addFiles('test/helpers.js');
-  api.addFiles('test/route_test.js');
-  api.addFiles('test/router_test.js');
-  api.addFiles('test/route_controller_test.js');
+  api.add_files('test/helpers.js');
+  api.add_files('test/route_test.js');
+  api.add_files('test/router_test.js');
+  api.add_files('test/route_controller_test.js');
 });
