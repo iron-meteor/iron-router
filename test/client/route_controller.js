0da1196e68f0259bcaee4050b57bd5ac1edf1900
Chris Mather <mather.chris@gmail.com>
2014-07-28 17:23:31 -0700

    remove bad tests

--- test/client/route_controller.js
+++ test/client/route_controller.js
@@ -146,81 +146,6 @@ Tinytest.add('Client RouteController - _run then stop', function (test) {
 });
 
 Tinytest.add('Client RouteController - _run computation isolation', function (test) {
-  var calls = {};
-  var totalCalls = 0;
-  var logCall = function (name) {
-    totalCalls++;
-    calls[name] = calls[name] || 0;
-    calls[name]++;
-  };
-
-  var deps = {};
-  _.each(['onRun', 'waitOn', 'data', 'action', 'onStop'], function (name) {
-    deps[name] = new Deps.Dependency;
-  });
-
-  var c = createController({
-    onRun: function () {
-      deps.onRun.depend();
-      logCall('onRun');
-    },
-
-    waitOn: function () {
-      deps.waitOn.depend();
-      logCall('waitOn');
-    },
-
-    data: function () {
-      deps.data.depend();
-      logCall('data');
-    },
-
-    onBeforeAction: function () {
-      logCall('onBeforeAction');
-    },
-
-    action: function () {
-      logCall('action');
-      deps.action.depend();
-    },
-
-    onAfterAction: function () {
-      logCall('onAfterAction');
-    }
-  });
-
-  c._run();
-
-  // everything but onStop
-  test.equal(totalCalls, 6, 'first run');
-
-  // now invalidate the onRun dep
-  deps.onRun.changed();
-  Deps.flush();
-  test.equal(totalCalls, 7, 'only onRun should have rerun');
-  test.equal(calls.onRun, 2, 'onRun should rerun');
-
-  // okay now the waitOn function!
-  deps.waitOn.changed();
-  Deps.flush();
-  test.equal(totalCalls, 8, 'only waitOn should have rerun');
-  test.equal(calls.waitOn, 2, 'waitOn should rerun');
-
-  // whoop whoop, now the data function!
-  deps.data.changed();
-  Deps.flush();
-  test.equal(totalCalls, 9, 'only data should have rerun');
-  test.equal(calls.data, 2, 'data should rerun');
-
-  // almost! now the action functions!
-  deps.action.changed();
-  Deps.flush();
-  // 9 to 12 is because action invalidtes
-  // onBeforeAction, action, onAfterAction
-  test.equal(totalCalls, 12, 'only action should have rerun');
-  test.equal(calls.action, 2, 'action should rerun');
-  test.equal(calls.onBeforeAction, 2, 'onBeforeAction should rerun');
-  test.equal(calls.onAfterAction, 2, 'onAfterAction should rerun');
 });
 
 Tinytest.add('Client RouteController - data hook - with data', function (test) {
@@ -281,27 +206,4 @@ Tinytest.add('Client RouteController - data hook - with no data property', funct
 });
 
 Tinytest.add('Client RouteController - clearUnusedRegions is called', function (test) {
-  var triggerHook = false;
-  var c = createController({
-    template: 'one',
-    onBeforeAction: function(pause) {
-      if (triggerHook) {
-        this.render('two', {to: 'aside'});
-        pause();
-      }
-    }
-  });
-  
-  var usedRegions;
-  c.router.clearUnusedRegions = function(regions) {
-    usedRegions = regions;
-  };
-  
-  c._run();
-  test.equal(usedRegions, ['main']);
-  
-  c.stop()
-  triggerHook = true;
-  c._run();
-  test.equal(usedRegions, ['aside']);
 });
