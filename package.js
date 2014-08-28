a8fb552d31a329d6622cdd217ea871f11cd5ae30
Chris Mather <mather.chris@gmail.com>
2014-08-27 21:53:57 -0700

    fix package.js to use METEOR vs METEOR-CORE in versionsFrom

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR-CORE@0.9.1');
+  api.versionsFrom('METEOR@0.9.1');
 
   api.use('reactive-dict', ['client', 'server']);
   api.use('deps', ['client', 'server']);
