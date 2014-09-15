c207d642e56de9bb3cb1552751149fd3ecec1ac2
Chris Mather <mather.chris@gmail.com>
2014-09-15 15:02:48 -0700

    add subscriptions option and change waitOn semantics
    Issue #819
    
    A new option called "subscriptions" can be used to call subscriptions
    before any other action functions are run. The return value is added to
    the wait list.
    
    The "waitOn" option is called next. It also adds its return value to the
    waitlist but has the added effect of using the "loading" hook
    automatically. If a waitOn option is provided, the loading hook will be
    automatically appended right before the action function so it will
    always be the last "before" hook to run before the action function.

--- package.js
+++ package.js
@@ -46,10 +46,10 @@ Package.on_use(function (api) {
   api.add_files('lib/route_controller_client.js', 'client');
   api.add_files('lib/route.js');
   api.add_files('lib/router.js');
-  api.add_files('lib/router_client.js', 'client');
-  api.add_files('lib/router_server.js', 'server');
   api.add_files('lib/hooks_client.js', 'client');
   api.add_files('lib/helpers_client.js', 'client');
+  api.add_files('lib/router_client.js', 'client');
+  api.add_files('lib/router_server.js', 'server');
   api.add_files('lib/plugins.js');
   api.add_files('lib/global_router.js');
   api.add_files('lib/templates.html');
